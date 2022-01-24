cefd5418aadc3b176a1492f36dccd53f30d60a66
tetosama <39564369+tetosama@users.noreply.github.com>
2022-01-23 17:29:30 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+Author: {FirstName LastName} [{UA Email}]  
+Course: [433 or 533 ?]  
+Date: Jan. [18], 2022
+
+**PLEASE UPDATE THIS README TO INCLUDE:**
+* a text description of how to run your program, 
+* document any idiosyncrasies, behaviors, or bugs of note that you want us to be aware of when grading, and
+* any other comments that you feel are relevant.
+
+Executing program:
+
+
+Description:
+
+
+Included files (**PLEASE ADD/UPDATE THIS LIST**):
+* index.html    -- a sample html file with a canvas
+* a01.js        -- a sample javascript file for functionality with the image uploading, and a method to parse PPM images
+* bunny.ppm     -- a test image
+
+
+**PLEASE PROVIDE ANY ATTRIBUTION HERE**
+* Images obtained from the following sources:
+  * bunny: http://graphics.stanford.edu/data/3Dscanrep/  
---
+++ a01.js
@@ -0,0 +1,101 @@
+/*
+  Basic File I/O for displaying
+  Skeleton Author: Joshua A. Levine
+  Modified by: Amir Mohammad Esmaieeli Sikaroudi
+  Email: amesmaieeli@email.arizona.edu
+  */
+
+
+//access DOM elements we'll use
+var input = document.getElementById("load_image");
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+var ppm_img_data;
+
+//Function to process upload
+var upload = function () {
+    if (input.files.length > 0) {
+        var file = input.files[0];
+        console.log("You chose", file.name);
+        if (file.type) console.log("It has type", file.type);
+        var fReader = new FileReader();
+        fReader.readAsBinaryString(file);
+
+        fReader.onload = function(e) {
+            //if successful, file data has the contents of the uploaded file
+            var file_data = fReader.result;
+            parsePPM(file_data);
+        }
+
+        /*
+        * TODO: ADD CODE HERE TO DO 2D TRANSFORMATION and ANIMATION
+        * Modify any code if needed
+        * Hint: Write a rotation method, and call WebGL APIs to reuse the method for animation
+        */
+
+
+    }
+}
+
+// Load PPM Image to Canvas
+function parsePPM(file_data){
+    /*
+   * Extract header
+   */
+    var format = "";
+    var width = 0;
+    var height = 0;
+    var max_v = 0;
+    var lines = file_data.split(/#[^\n]*\s*|\s+/); // split text by whitespace or text following '#' ending with whitespace
+    var counter = 0;
+    // get attributes
+    for(var i = 0; i < lines.length; i ++){
+        if(lines[i].length == 0) {continue;} //in case, it gets nothing, just skip it
+        if(counter == 0){
+            format = lines[i];
+        }else if(counter == 1){
+            width = lines[i];
+        }else if(counter == 2){
+            height = lines[i];
+        }else if(counter == 3){
+            max_v = Number(lines[i]);
+        }else if(counter > 3){
+            break;
+        }
+        counter ++;
+    }
+    console.log("Format: " + format);
+    console.log("Width: " + width);
+    console.log("Height: " + height);
+    console.log("Max Value: " + max_v);
+    /*
+     * Extract Pixel Data
+     */
+    var bytes = new Uint8Array(3 * width * height);  // i-th R pixel is at 3 * i; i-th G is at 3 * i + 1; etc.
+    // i-th pixel is on Row i / width and on Column i % width
+    // Raw data must be last 3 X W X H bytes of the image file
+    var raw_data = file_data.substring(file_data.length - width * height * 3);
+    for(var i = 0; i < width * height * 3; i ++){
+        // convert raw data byte-by-byte
+        bytes[i] = raw_data.charCodeAt(i);
+    }
+    // update width and height of canvas
+    document.getElementById("canvas").setAttribute("width", window.innerWidth);
+    document.getElementById("canvas").setAttribute("height", window.innerHeight);
+    // create ImageData object
+    var image_data = ctx.createImageData(width, height);
+    // fill ImageData
+    for(var i = 0; i < image_data.data.length; i+= 4){
+        let pixel_pos = parseInt(i / 4);
+        image_data.data[i + 0] = bytes[pixel_pos * 3 + 0]; // Red ~ i + 0
+        image_data.data[i + 1] = bytes[pixel_pos * 3 + 1]; // Green ~ i + 1
+        image_data.data[i + 2] = bytes[pixel_pos * 3 + 2]; // Blue ~ i + 2
+        image_data.data[i + 3] = 255; // A channel is deafult to 255
+    }
+    ctx.putImageData(image_data, canvas.width/2 - width/2, canvas.height/2 - height/2);
+    ppm_img_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+}
+
+//Connect event listeners
+input.addEventListener("change", upload);
